d585ce83726bef99223d0fad24281e212af5f3a6
Ozodbek <o.jumaev@safenetpay.com>
2022-02-07 19:36:25 +0500

    create list and $emit

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <form action="#" @submit.prevent>
+      <div class="col-sm-5" style="margin: 0 auto">
+        <input
+            v-model="comment.name"
+            type="text"
+            class="form-control" placeholder="Name">
+      </div>
+      <div class="col-sm-5" style="margin: 0 auto">
+        <input
+            v-model="comment.email"
+            type="email"
+            class="form-control" style="margin: 10px 0;" placeholder="E-mail">
+      </div>
+      <div class="col-sm-5" style="margin: 0 auto">
+        <input
+            v-model="comment.content"
+            type="text"
+            class="form-control" placeholder="Content">
+      </div>
+      <button @click="createComment" class="btn btn-success" style="margin-top: 12px">Add</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommentForm",
+  data() {
+   return {
+     comment: {
+       name: '',
+       email: '',
+       content: ''
+     }
+    }
+  },
+  methods: {
+    createComment() {
+      this.comment.id = Date.now();
+      this.$emit('addComment', this.comment);
+      this.comment = {
+        name: '',
+        email: '',
+        content: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CommentList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="nav" style="width: 100%;max-width: 768px; margin: 30px auto 0">
+    <li class="nav-item col-sm-5"
+        style="width: 100%; text-align: left;padding: 12px;border: 1px solid grey;margin-bottom: 10px"
+        v-for="comment in comments" :key="comment.id">
+      <b>Name:</b> <span>{{comment.name}}</span> <br>
+      <b>Email:</b> <span>{{comment.email}}</span> <br>
+      <b>Content:</b> <span>{{comment.content}}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CommentList",
+  props: {
+    comments: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
